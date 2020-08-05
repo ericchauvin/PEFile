4a54fc71a463d18c149ba385706a8764d43dda8d
Eric Chauvin <chauvineric4@gmail.com>
2020-08-05 12:32:49 -0600

    Create ViewPEFile.java

---
+++ ViewPEFile.java
@@ -0,0 +1,16 @@
+// Copyright Eric Chauvin 2020.
+
+// The main.exe file in Cpp is over 41,000 bytes.
+// Read that to a byte array.
+
+// Make a PE file as strings so it's a readable
+// PE file, then make that into the binary PE file.
+
+// For Linux it's the ELF format.
+// Executable and Linkable Format
+
+
+public class ViewPEFile
+  {
+
+  }
