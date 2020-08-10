3577f2fa02adb484b2d69f21b5c9a898c9085e69
Eric Chauvin <chauvineric4@gmail.com>
2020-08-10 13:14:57 -0600

    Update MainApp.java

--- MainApp.java
+++ MainApp.java
@@ -17,7 +17,7 @@ import javax.swing.SwingUtilities;
 
 class MainApp implements Runnable
   {
-  public static final String versionDate = "8/5/2020";
+  public static final String versionDate = "8/10/2020";
   private MainWindow mainWin;
   // public ConfigureFile mainConfigFile;
   private String[] argsArray;
